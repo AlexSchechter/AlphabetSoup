619c73e59e33dfa3391d3b0e8bef2aff24ea6cd2
AlexSchechter <a.schechter@outlook.com>
2016-01-02 13:53:34 +0000

    Fully Functional Version

---
+++ AlphabetSoup/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetSoup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string: ");
+            List<char> orderedString = MakeAlphabetSoup(Console.ReadLine());
+            orderedString.ForEach(Console.Write);
+            Console.ReadKey();
+        }
+
+        private static List<char> MakeAlphabetSoup(string word)
+        {
+            List<char> wordAsList = word.ToList();
+            wordAsList.Sort();
+            return wordAsList;
+        }
+
+    }
+}
